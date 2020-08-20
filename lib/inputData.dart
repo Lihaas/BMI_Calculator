5a23c85cf2b51458918ae49922a173713e5d9ed3
Sahil Verma <vermasahilcf5@gmail.com>
2020-08-20 17:32:35 +0530

    UI Design

--- lib/inputData.dart
+++ lib/inputData.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'IconContanor.dart';
 import 'ReuseableCard.dart';
+
+enum Gender {
+  male,
+  female,
+}
+
+Gender SelectGender;
+
+Color ActiveColor = Color(0xFF9c0000);
+Color UnActiveCardColor = Color(0xFFd50000);
+int height = 60;
+
+double BottomHeight = 50;
 
 class InputData extends StatefulWidget {
   @override
@@ -11,10 +25,6 @@ class InputData extends StatefulWidget {
 }
 
 class _InputDataState extends State<InputData> {
-  double BottomHeight = 60;
-  Color UnActiveCardColor = Color(0xFFd50000);
-  Color ActiveColor = Color(0xFF9b0000);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,20 +39,38 @@ class _InputDataState extends State<InputData> {
               child: Row(
                 children: <Widget>[
                   Expanded(
-                    child: ReuseableCard(
-                      colors: UnActiveCardColor,
-                      cardMake: IconContanor(
-                        icon: FontAwesomeIcons.venus,
-                        label: "FEMALE",
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          SelectGender = Gender.female;
+                        });
+                      },
+                      child: ReuseableCard(
+                        colors: SelectGender == Gender.female
+                            ? ActiveColor
+                            : UnActiveCardColor,
+                        cardMake: IconContanor(
+                          icon: FontAwesomeIcons.venus,
+                          label: "FEMALE",
+                        ),
                       ),
                     ),
                   ),
                   Expanded(
-                    child: ReuseableCard(
-                      colors: UnActiveCardColor,
-                      cardMake: IconContanor(
-                        icon: FontAwesomeIcons.mars,
-                        label: "MALE",
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          SelectGender = Gender.male;
+                        });
+                      },
+                      child: ReuseableCard(
+                        colors: SelectGender == Gender.female
+                            ? UnActiveCardColor
+                            : ActiveColor,
+                        cardMake: IconContanor(
+                          icon: FontAwesomeIcons.mars,
+                          label: "MALE",
+                        ),
                       ),
                     ),
                   )
@@ -50,16 +78,125 @@ class _InputDataState extends State<InputData> {
               ),
             ),
             Expanded(
-              child: ReuseableCard(colors: UnActiveCardColor),
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    print("2");
+                  });
+                },
+                child: ReuseableCard(
+                  colors: UnActiveCardColor,
+                  cardMake: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Center(
+                        child: Container(
+                          margin: EdgeInsets.only(
+                              left: 0, top: 10, right: 0, bottom: 0),
+                          child: Text(
+                            "Body Height",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Center(
+                        child: Text(
+                          height.toString(),
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      buildSlider()
+                    ],
+                  ),
+                ),
+              ),
             ),
             Expanded(
               child: Row(
                 children: <Widget>[
                   Expanded(
-                    child: ReuseableCard(colors: UnActiveCardColor),
+                    child: ReuseableCard(
+                      colors: UnActiveCardColor,
+                      cardMake: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                            child: Text(
+                              "Weight",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 18),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                            child: Text(
+                              "38" + " KG",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Expanded(
+                                child:
+                                    RounderButton(icon: FontAwesomeIcons.minus),
+                              ),
+                              Expanded(
+                                child:
+                                    RounderButton(icon: FontAwesomeIcons.plus),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                   Expanded(
-                    child: ReuseableCard(colors: UnActiveCardColor),
+                    child: ReuseableCard(
+                      colors: UnActiveCardColor,
+                      cardMake: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                            child: Text(
+                              "Weight",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 18),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                            child: Text(
+                              "38" + " KG",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Expanded(
+                                child:
+                                    RounderButton(icon: FontAwesomeIcons.minus),
+                              ),
+                              Expanded(
+                                child:
+                                    RounderButton(icon: FontAwesomeIcons.plus),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
                   )
                 ],
               ),
@@ -71,6 +208,41 @@ class _InputDataState extends State<InputData> {
           ],
         ),
       ),
+    );
+  }
+
+  Slider buildSlider() {
+    return Slider(
+      value: height.toDouble(),
+      min: 20,
+      max: 200,
+      label: height.round().toString(),
+      onChanged: (double value) {
+        setState(() {
+          height = value.round();
+        });
+      },
+    );
+  }
+}
+
+class RounderButton extends StatelessWidget {
+  RounderButton({@required this.icon});
+
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Icon(
+        icon,
+        color: Colors.white,
+      ),
+      onPressed: () {},
+      elevation: 6,
+      constraints: BoxConstraints.tightFor(width: 50, height: 50),
+      shape: CircleBorder(),
+      fillColor: Colors.red,
     );
   }
 }
